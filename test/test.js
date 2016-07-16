5063499e36bcc38dc6a0b8e5d5347d2448008005
Ikenna <ikennaugwuh@gmail.com>
2016-07-16 19:19:59 -0400

    Got some initial tests to pass.

--- test/test.js
+++ test/test.js
@@ -2,20 +2,31 @@
 import chai from 'chai';
 
 // Import Any Files to Test
-import { MemoryMatch } from '../src/js/classes/game'
+import MemoryMatch from '../src/js/classes/game';
 
 // Set Chai Constants
 const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('About the game Memory Match', ()=>{
-
-    describe('The creation of a Memory Match game', ()=>{
-        it('should be an instance of MemoryMatch', ()=>{
-            let game = new MemoryMatch();
-            expect(game).to.be.an.instanceof(MemoryMatch);
-        });
+describe('About the game Memory Match', function(){
+    let game;
+    beforeEach(function(){
+        game = new MemoryMatch();
     });
 
+    describe('The creation of a Memory Match game', function(){
+        it('should be an instance of MemoryMatch', ()=>{
+            expect(game).to.be.an.instanceof(MemoryMatch);
+        });
+        it('must have multiple modes', ()=>{
+            expect(game.mode).to.be.an('array');
+            describe('Easy Mode', function(){
+                it('should have a grid of size 3x3 with a free square', ()=>{
+                    game.setMode('easy');
+                    expect(game.grid.length).to.equal(8);
+                });
+            });
+        });
+    });
 });
