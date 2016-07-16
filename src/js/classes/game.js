045ea94e115a8d45b3e59a8a8ca4b898d009c264
Ikenna <ikennaugwuh@gmail.com>
2016-07-16 19:18:59 -0400

    Edited the game class with a new method to set the game mode.

--- src/js/classes/game.js
+++ src/js/classes/game.js
@@ -1,5 +1,33 @@
-class MemoryMatch {
-    let game = this;
+import _ from 'lodash';
+
+class Game {
+    constructor(){
+        let game = this;
+        /*
+        // game.emoji = [😎,😍,👊,🔥,🌮,🌭,🦄,🌍,🦁,🤘,🙉,🙊,👌,👍,👥,💵,💎,💋,🎩,👑,✨,😈,😂,💰,💖,💣,🍕,🍋,🏀,🏈,🎱,🔑,💡,🔮,💯,🌧,🗿,📍,💈,⭐️]
+        */
+        game.emoji = new Array(40);
+        game.grid = [];
+        game.matches = [];
+        game.mode = [];
+    }
+    setMode(mode){
+        let game = this;
+        switch (mode) {
+            case 'easy' :
+                game.grid = game.matches = _.sampleSize(game.emoji, 4);
+                game.grid = game.matches.concat(game.grid);
+                break;
+            case 'medium' :
+                break;
+            case 'hard' :
+                break;
+            case 'crazy' :
+                break;
+            case 'insane' :
+                break;
+        }
+    }
 }
 
-export { MemoryMatch };
+export default Game;
