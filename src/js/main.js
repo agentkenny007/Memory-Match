d55ff21185ed9ec481ab7520fa99ae8983cb692a
Ikenna <ikennaugwuh@gmail.com>
2016-07-17 01:16:50 -0400

    Started working on some js.

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+import MemoryMatch from './classes/game';
+
+const   GRID = $('.game-grid ul'),
+        CARD = $('<li class="card"><img src="http://placehold.it/1"></li>');
