63213da4710ada44012f7d2b53e6f8d1e4d0d4c7
Ikenna <ikennaugwuh@gmail.com>
2016-07-17 15:12:22 -0400

    Edited a splash page for the game.

--- src/js/classes/card.js
+++ src/js/classes/card.js
@@ -1,7 +1,8 @@
 class Card {
-    constructor(id, emoji){
-        this.id = id || null;
+    constructor(id, emoji, index){
+        this.id = id || id === 0 ? id : null;
         this.emoji = emoji || null;
+        this.index = index || null;
     }
 }
 
