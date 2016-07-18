d7e615078cf056891fcffd307d7342fa6dccad56
Ikenna <ikennaugwuh@gmail.com>
2016-07-18 00:15:01 -0400

    Added a bonus option to the scoreboard class.

--- src/js/classes/score.js
+++ src/js/classes/score.js
@@ -2,9 +2,10 @@ class ScoreBoard {
     constructor(options){
         options = options || {};
         let board = this;
+        board.bonus = options.bonus || 0;
         board.faulty = options.faulty || 0;
         board.guessed = options.guessed || 0;
-        board.high = options.high;
+        board.high = options.high || null;
         board.played = options.played || 0;
         board.score = options.score || 20;
     }
